81b35d54d8d441777cb72c51667a44d17fe4221c
rbonifacio <rbonifacio123@gmail.com>
2024-05-29 16:07:04 -0300

    fix for the enhanced property assignment + function property

--- src/main/java/br/unb/cic/js/miner/JSVisitor.java
+++ src/main/java/br/unb/cic/js/miner/JSVisitor.java
@@ -35,7 +35,6 @@ public class JSVisitor extends JavaScriptParserBaseVisitor<Void> {
 			SpreadArguments,
 			OptionalChain,
 			TemplateStringExpressions,
-			ObjectProperties,
 			RegularExpressions,
 			NullCoalesceOperators,
 			HashBangLines,
@@ -43,7 +42,9 @@ public class JSVisitor extends JavaScriptParserBaseVisitor<Void> {
 			PrivateFields,
 			NumericLiteralSeparators,
 			BigInt,
-			ComputedProperties,
+			ComputedPropertyAssignments,
+			EnhancedPropertyAssignments,
+			FunctionPropertyDeclarations,
 			Statements
 	}
 	
@@ -88,7 +89,6 @@ public class JSVisitor extends JavaScriptParserBaseVisitor<Void> {
 	AtomicInteger totalSpreadArguments = new AtomicInteger(0);
 	AtomicInteger totalOptionalChain = new AtomicInteger(0);
 	AtomicInteger totalTemplateStringExpressions = new AtomicInteger(0);
-	AtomicInteger totalObjectProperties = new AtomicInteger(0);
 	AtomicInteger totalRegularExpressions = new AtomicInteger(0);
 	AtomicInteger totalNullCoalesceOperators = new AtomicInteger(0);
 	AtomicInteger totalHashBangLines = new AtomicInteger(0);
@@ -96,7 +96,9 @@ public class JSVisitor extends JavaScriptParserBaseVisitor<Void> {
 	AtomicInteger totalPrivateFields = new AtomicInteger(0);
 	AtomicInteger totalNumericLiteralSeparators = new AtomicInteger(0);
 	AtomicInteger totalBigInt = new AtomicInteger(0);
-	AtomicInteger totalComputedProperties = new AtomicInteger(0);
+	AtomicInteger totalComputedPropertyAssignments = new AtomicInteger(0);
+	AtomicInteger totalEnhancementPropertyAssigments = new AtomicInteger(0);
+	AtomicInteger totalFunctionPropertyDeclarations = new AtomicInteger(0);
 	AtomicInteger totalStatements = new AtomicInteger(0);
 	
 	
@@ -157,8 +159,8 @@ public class JSVisitor extends JavaScriptParserBaseVisitor<Void> {
 			totalAsyncDeclarations.incrementAndGet();
 			changeFilesOccurrences(Feature.AsyncDeclarations);
 		}
-		totalObjectProperties.incrementAndGet();
-		changeFilesOccurrences(Feature.ObjectProperties);
+		totalFunctionPropertyDeclarations.incrementAndGet();
+		changeFilesOccurrences(Feature.FunctionPropertyDeclarations);
 		return super.visitFunctionProperty(ctx);
 	}
 
@@ -232,8 +234,8 @@ public class JSVisitor extends JavaScriptParserBaseVisitor<Void> {
 			totalSpreadArguments.incrementAndGet();
 			changeFilesOccurrences(Feature.SpreadArguments);
 		}else{
-			totalObjectProperties.incrementAndGet();
-			changeFilesOccurrences(Feature.ObjectProperties);
+			totalEnhancementPropertyAssigments.incrementAndGet();
+			changeFilesOccurrences(Feature.EnhancedPropertyAssignments);
 		}
 		return super.visitPropertyShorthand(ctx);
 	}
@@ -369,8 +371,8 @@ public class JSVisitor extends JavaScriptParserBaseVisitor<Void> {
 
 	@Override
 	public Void visitComputedPropertyExpressionAssignment(ComputedPropertyExpressionAssignmentContext ctx) {
-		totalComputedProperties.incrementAndGet();
-		changeFilesOccurrences(Feature.ComputedProperties);
+		totalComputedPropertyAssignments.incrementAndGet();
+		changeFilesOccurrences(Feature.ComputedPropertyAssignments);
 		return super.visitComputedPropertyExpressionAssignment(ctx);
 	}
 
