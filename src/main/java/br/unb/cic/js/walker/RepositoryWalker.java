b8c687590e6464355b815d5a5f84c45d14fc169d
Fausto Carvalho <faustocarva@gmail.com>
2024-12-30 16:59:32 -0300

    new feature is collected: for await of

--- src/main/java/br/unb/cic/js/walker/RepositoryWalker.java
+++ src/main/java/br/unb/cic/js/walker/RepositoryWalker.java
@@ -318,7 +318,7 @@ public final class RepositoryWalker {
             metrics.add(Metric.builder().name("forof_statements_files").value(visitor.getTotalForOfStatements()).build());
             metrics.add(Metric.builder().name("forin_statements_files").value(visitor.getTotalForInStatements()).build());
             
-            metrics.add(Metric.builder().name("forawaitof_files").value(visitor.getTotalForOfStatements()).build());
+            metrics.add(Metric.builder().name("forawaitof_files").value(visitor.getTotalForAwaitOf()).build());
 
             summary.date(current).revision(head.toString()).metrics(metrics).errors(errors);
         } catch (Exception ex) {
