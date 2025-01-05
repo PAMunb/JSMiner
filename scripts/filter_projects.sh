6eb9842929e78bb4beb94cd4acd59352dffa6831
waltim <waltimlmm@gmail.com>
2025-01-05 15:15:11 -0300

    bkp para versão antes da review

--- scripts/filter_projects.sh
+++ scripts/filter_projects.sh
@@ -10,8 +10,6 @@ output_file="filtered-results.csv"
 
 # Lista de nomes de projetos a serem removidos
 # 32 por checkout-conflicts and exceptions
-# projects="bigbluebutton|biwascheme|browserify|canjs|cartodb|connect-api-examples|e2openplugin-openwebif|etherpad-lite|geonode|iteexe|jw-community|mediathread|node-tap|one|opendsa|paper.js|patterns|prey-node-client|qooxdoo|salesforcemobilesdk-shared|sequenceserver|tapjs|www.tjvantoll.com|zotero|acorn|rhino|hue|cesium|limesurvey|countly-server|erp5|releases-comm-central"
-
 projects="bigbluebutton|biwascheme|browserify|canjs|cartodb|connect-api-examples|e2openplugin-openwebif|etherpad-lite|geonode|iteexe|jw-community|mediathread|node-tap|one|opendsa|paper.js|patterns|prey-node-client|qooxdoo|salesforcemobilesdk-shared|sequenceserver|tapjs|www.tjvantoll.com|zotero|acorn|rhino|hue|cesium|limesurvey|countly-server|erp5|releases-comm-central|copenhagenjs.dk|compromise|openstreetbrowser|nightwatch|jsbin|tal|wirecloud|todomvc|framework|tilemill|windshaft-cartodb|node-restify|rainwave|ably-js|svg.js|windshaft|underscore|prose|async|knex|vimium|webplotdigitizer|pyret-lang|recurly-js|ourumbraco|profiler"
 
 # Filtra as linhas que não correspondem ao padrão
